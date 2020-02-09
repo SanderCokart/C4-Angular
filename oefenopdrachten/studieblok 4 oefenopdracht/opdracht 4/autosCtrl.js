1bb27e97301bd3d27e3be796e8f4871c327c51b2
Sander Cokart <cokart32@gmail.com>
2020-02-09 16:19:54 +0100

    semicolon toegevoegd

--- oefenopdrachten/studieblok 4 oefenopdracht/opdracht 4/autosCtrl.js
+++ oefenopdrachten/studieblok 4 oefenopdracht/opdracht 4/autosCtrl.js
@@ -5,7 +5,7 @@ autoApp.controller('autoController', function($scope) {
         {kenteken: '12412', kleur: 'geel', merk: 'bmw', aantalDeuren: 4},
         {kenteken: '87908', kleur: 'oranje', merk: 'volvo', aantalDeuren: 4},
         {kenteken: '35345', kleur: 'blauw', merk: 'ford', aantalDeuren: 2}
-    ]
+    ];
 
     $scope.columns = Object.keys($scope.autos[0]);
 });
